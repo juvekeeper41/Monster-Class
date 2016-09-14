40e92605277a274a0c24bd8b3a87e88870db2f39
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-14 09:36:04 -0600

    Added start method.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,26 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
+	private MarshmallowMonster firstMonster;
 
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("Anubis", 7.2, 2, false, 3, 1);
+	}
+	
+	/*
+	 * name = Anubis
+	 * armCount = 3
+	 * eyeCount = 2
+	 * antennaCount = 7.2
+	 * hasBellyButton = false
+	 * noseCount = 1
+	 */
+	public void start()
+	{
+		System.out.println("Here is my monster: " + firstMonster);
+	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,44 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	
+	private String name;
+	private double antennaCount;
+	private int eyeCount;
+	private boolean hasBellyButton;
+	private int armCount;
+	private int noseCount;
+	
+	public	MarshmallowMonster()
+	{
+		this.name = "no name here";
+		this.antennaCount = -23423.131235;
+		this.eyeCount = -2345678;
+		this.hasBellyButton = false;
+		this.armCount = -32436;
+		this.noseCount = -7765;
+	}
+				
+		public MarshmallowMonster(String name, 
+				double antennaCount, 
+					int eyeCount, 
+						boolean hasBellyButton, 
+							int armCount, 
+								int noseCount)
+	{
+		this.name = name;
+		this.antennaCount = antennaCount;
+		this.eyeCount = eyeCount;
+		this.hasBellyButton = hasBellyButton;
+		this.armCount = armCount;
+		this.noseCount = noseCount;
+	}
+	
+	
+		public String toString()
+		{
+			String description = "Name is " + this.name;
+			
+			return description;
+		}
 }
