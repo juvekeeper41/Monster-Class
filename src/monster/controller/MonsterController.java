bb1f54c5b2bd439d909c3a331787e3502251eeca
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-16 08:54:40 -0600

    We edited the System.out and added functions to the code.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,14 +1,17 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	private MarshmallowMonster firstMonster;
+	private Scanner keyboardInput;
 
 	public MonsterController()
 	{
 		firstMonster = new MarshmallowMonster("Anubis", 7.2, 2, false, 3, 1);
+		keyboardInput = new Scanner(System.in);
 	}
 	
 	/*
@@ -27,7 +30,23 @@ public class MonsterController
 		System.out.println("Does not have a belly button,");
 		System.out.println(firstMonster.getArmCount() + " arms,");
 		System.out.println("and " + firstMonster.getNoseCount() + " nose.");
+		
+		System.out.println("Do you want to change my name?");
+		String answer = keyboardInput.nextLine();
+		
+		if (answer.equalsIgnoreCase("yes"))
+		{
+			System.out.println("What do you want my name to be?");
+			String newName = keyboardInput.nextLine();
+			firstMonster.setName(newName);
+		
+		System.out.println(firstMonster);
+		}
+		else
+		{
+			System.out.println("Fine, don't change my name!");
+		}
+		System.out.println(firstMonster);
 	}
-	
 }
 
